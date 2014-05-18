0e4951caaef10474f49eefcdafcee6bff5a5c165
Luka Mirosevic <luka@goonbee.com>
2014-05-18 17:41:45 +0200

    Pushed functionality up into shared

--- GoonbeePushService.thrift
+++ GoonbeePushService.thrift
@@ -6,37 +6,10 @@
 //  Copyright (c) 2014 Goonbee. All rights reserved.
 //
 
-include "shared/GoonbeeSharedThriftService.thrift"
-
+include "shared/GoonbeeShared.thrift"
 
 namespace js GBPushService
 namespace cocoa GBPush
-
-
-enum ResponseStatus {
-    SUCCESS =               0,
-    GENERIC =               1,
-    MALFORMED_REQUEST =     2,
-    AUTHENTICATION =        3,
-    AUTHORIZATION =         4,
-    PHASED_OUT =            5,
-}
-
-exception RequestError {
-    1: ResponseStatus       status,
-    2: optional string      message,
-}
-
-enum RangeDirection {
-    FORWARDS =              0,
-    BACKWARDS =             1,
-}
-
-struct Range {
-    1: RangeDirection       direction,
-    2: i32                  index,
-    3: i32                  length,
-}
 
 enum PushTokenType {
     APNS =                  0,
@@ -48,20 +21,19 @@ struct PushToken {
     2: string               token,
 }
 
-
-service GoonbeePushService extends GoonbeeSharedThriftService.GoonbeeSharedThriftService {
+service GoonbeePushService extends GoonbeeShared.BaseService {
     /**
      * Subscribe or unsubscribe from a particular channel. Idempotent.
      */
-    void                setChannelSubscriptionStatus    (1: PushToken pushToken, 2: string channel, 3: bool subscriptionStatus)
+    void                setChannelSubscriptionStatus    (1: PushToken pushToken, 2: string channel, 3: bool subscriptionStatus)         throws(1: GoonbeeShared.RequestError error),
 
     /**
      * Get list of channels the user is subscribed to.
      */
-    list<string>        subsriptions                    (1: PushToken pushToken, 2: Range range)
+    list<string>        subscribedChannels              (1: PushToken pushToken, 2: GoonbeeShared.Range range)                          throws(1: GoonbeeShared.RequestError error),
 
     /**
      * Check whether the user is subscribed on a particular channel or not.
      */
-    bool                subsriptionStatus               (1: PushToken pushToken, 2: string channel)
+    bool                subsriptionStatus               (1: PushToken pushToken, 2: string channel)                                     throws(1: GoonbeeShared.RequestError error),
 }
